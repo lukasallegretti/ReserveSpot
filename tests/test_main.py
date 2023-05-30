8daa1f5b2975462521191254a0bada141096892c
lukasallegretti <lukasallegretti@outlook.com>
2023-05-29 21:33:11 -0300

    fix: corrigindo a chamada do teste

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,6 +2,6 @@ from fastapi.testclient import TestClient
 
 
 def test_enpoint(http_client: TestClient):
-    response = http_client.test_client().get("/")
+    response = http_client.get("/")
     assert response.status_code == 200
     assert response.json() == {"Hello": "World"}
